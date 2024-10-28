28a3b2103f8b5516005c12725e036799802d8698
EvertonJunior <devjoseeverton@gmail.com>
2024-10-28 15:37:32 -0300

    metodos em UsuarioController, findById, findAll, deleteById

--- src/main/java/com/ej/msuser/web/controllers/UsuarioController.java
+++ src/main/java/com/ej/msuser/web/controllers/UsuarioController.java
@@ -5,14 +5,14 @@ import com.ej.msuser.service.UsuarioService;
 import com.ej.msuser.web.dtos.UsuarioCreateDto;
 import com.ej.msuser.web.dtos.UsuarioResponseDto;
 import com.ej.msuser.web.mappers.UsuarioMapper;
+import com.oracle.svm.core.annotate.Delete;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/usuarios")
@@ -27,5 +27,20 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(usuario));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDto> findById(@PathVariable long id){
+        return ResponseEntity.ok(UsuarioMapper.toDto(service.findById(id)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> findAll(){
+        return ResponseEntity.ok(service.findAll().stream().map(UsuarioMapper::toDto).toList());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
